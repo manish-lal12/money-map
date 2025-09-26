84387516e12184d273c3f0ab7bd008e828d425af
Manish Kumar <126171292+manish-lal12@users.noreply.github.com>
2025-09-26 11:52:23 +0530

    Delete unrequired files from directory

--- src/prisma/migrations/20250511234249_changed/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "FinancialGoals" ALTER COLUMN "description" DROP NOT NULL;
--- src/prisma/migrations/20250512021048_removed/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `name` on the `Savings` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Savings" DROP COLUMN "name";
